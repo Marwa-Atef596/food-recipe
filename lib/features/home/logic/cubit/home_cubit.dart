9d1a60d6c65a1008b4ecd2ad010176184f25e5af
Marwa Atef <marwaatef2001@gmail.com>
2025-09-02 18:13:31 +0300

    test development branch

---
+++ lib/features/home/logic/cubit/home_cubit.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: avoid_print, depend_on_referenced_packages
+
+import 'package:bloc/bloc.dart';
+import 'package:food_recipe/features/home/data/repo/home_repo.dart';
+
+import 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit(this._homeRepo) : super(HomeInitial());
+
+  final HomeRepo _homeRepo; 
+
+  Future<void> getMeals() async {
+    emit(HomeLoading());
+
+    final result = await _homeRepo.getRecipe();
+    result.fold(
+      (failure) {
+        print("Repo error: ${failure.message}");
+        emit(HomeFailure(failure.message));
+      },
+      (meals) {
+        print("Meals fetched successfully: ${meals.length} items");
+        for (var m in meals) {
+          print(m.name);
+        }
+        emit(HomeSucess(meals));
+      },
+    );
+  }
+}
